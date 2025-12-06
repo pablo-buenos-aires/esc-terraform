bcd5124074c23ca9a989d52d016265e49983a70f
pablo-buenos-aires <pp3842879@gmail.com>
2025-12-06 00:20:35 -0300

    2

---
+++ modules/esc/variables.tf
@@ -0,0 +1,21 @@
+variable "acm_certificate_arn" {
+  type        = string
+  description = "ARN сертификата ACM для api-домена"
+}
+variable "ecr_repository_url" {
+  type        = string
+  description = "ECR repo URL (без тега), типа 8369....dkr.ecr.sa-east-1.amazonaws.com/go-backend"
+}
+
+variable "image_tag" {
+  type        = string
+  default     = "latest"
+  description = "Docker image tag"
+}
+
+# БД и другие настройки как env-переменные
+variable "db_host"     { type = string }
+variable "db_port"     { type = string }
+variable "db_user"     { type = string }
+variable "db_password" { type = string }
+variable "db_name"     { type = string }
